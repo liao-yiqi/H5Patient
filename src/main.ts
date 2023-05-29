770d2883504e986985bdf8b5bceaca7dc77cf442
komorebiHl <821890952@qq.com>
2023-05-29 09:55:22 +0800

    feat:引入数据持久化插件

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import persist from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
 import "vant/lib/index.css";
 import "./styles/main.scss";
 const app = createApp(App);
-
-app.use(createPinia());
+const pinia = createPinia();
+//使用数据持久化插件
+pinia.use(persist);
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
