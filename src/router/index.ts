4b3e9dd69aed6d397ba0897a4a3987ad137fdae9
komorebiHl <821890952@qq.com>
2023-05-30 16:05:11 +0800

    feat:修改页面标题

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,21 +7,39 @@ const router = createRouter({
     {
       path: "/Login",
       name: "Login",
-      component: () => import("../views/Login/index.vue")
+      component: () => import("../views/Login/index.vue"),
+      meta: { title: "登录" }
     },
     {
       path: "/",
       component: () => import("@/views/Layout/index.vue"),
       redirect: "/home",
       children: [
-        { path: "/home", component: () => import("@/views/Home/index.vue") },
-        { path: "/article", component: () => import("@/views/Article/index.vue") },
-        { path: "/notify", component: () => import("@/views/Notify/index.vue") },
-        { path: "/user", component: () => import("@/views/User/index.vue") }
+        {
+          path: "/home",
+          component: () => import("@/views/Home/index.vue"),
+          meta: { title: "首页" }
+        },
+        {
+          path: "/article",
+          component: () => import("@/views/Article/index.vue"),
+          meta: { title: "健康百科" }
+        },
+        {
+          path: "/notify",
+          component: () => import("@/views/Notify/index.vue"),
+          meta: { title: "消息通知" }
+        },
+        {
+          path: "/user",
+          component: () => import("@/views/User/index.vue"),
+          meta: { title: "个人中心" }
+        }
       ]
     }
   ]
 });
+//前置路由守卫
 router.beforeEach((to) => {
   // 用户仓库
   const store = useUserStore();
@@ -30,5 +48,9 @@ router.beforeEach((to) => {
   // 如果没有登录且不在白名单内，去登录
   if (!store.user?.token && !wihteList.includes(to.path)) return "/login";
   // 否则不做任何处理
+});
+//后置路由守卫
+router.afterEach((to) => {
+  document.title = `${to.meta.title}-优医问诊`;
 });
 export default router;
