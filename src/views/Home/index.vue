6188d8b62e9995e81acc081d3ebbc84295921c66
komorebiHl <821890952@qq.com>
2023-06-04 11:11:33 +0800

    feat:建立问诊记录仓库

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,9 @@ import KnowledgeList from "./components/KnowledgeList.vue";
 import FollowDoctor from "./components/FollowDoctor.vue";
 // active 的值是 tab 的索引
 const active = ref(1);
+import { useConsultStore } from "@/stores/modules/consult";
+const store = useConsultStore();
+import { ConsultType } from "@/enums";
 </script>
 
 <template>
@@ -26,7 +29,7 @@ const active = ref(1);
           </router-link>
         </van-col>
         <van-col span="8">
-          <router-link to="/consult/fast" class="nav">
+          <router-link to="/consult/fast" class="nav" @click="store.setType(ConsultType.Fast)">
             <cp-icon name="home-graphic"></cp-icon>
             <p class="title">极速问诊</p>
             <p class="desc">20s医生极速回复</p>
