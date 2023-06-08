2e9cf482105d91af6a5d9f7acd60b3cf83e0ff0b
komorebiHl <821890952@qq.com>
2023-06-08 11:21:15 +0800

    feat:查看处方功能完成

--- src/views/Room/components/RoomMessage.vue
+++ src/views/Room/components/RoomMessage.vue
@@ -6,6 +6,8 @@ import { IllnessTime } from "@/enums";
 import { flagOptions, timeOptions } from "@/services/constants";
 import dayjs from "dayjs";
 import { useUserStore } from "@/stores";
+import { getPrescriptionPic } from "@/services/consult";
+import { showImagePreview } from "vant";
 const store = useUserStore();
 const formatTime = (time: string) => dayjs(time).format("HH:mm");
 defineProps<{ list: Message[] }>();
@@ -15,6 +17,13 @@ const getIllnessTimeText = (time: IllnessTime) =>
   timeOptions.find((item) => item.value === time)?.label;
 
 const getConsultFlagText = (flag: 0 | 1) => flagOptions.find((item) => item.value === flag)?.label;
+const buy = () => {};
+const showPrescription = async (id: string) => {
+  // 发请求获取地址
+  const { data } = await getPrescriptionPic(id);
+  // 进行预览
+  showImagePreview([data.url]);
+};
 </script>
 
 <template>
@@ -60,7 +69,6 @@ const getConsultFlagText = (flag: 0 | 1) => flagOptions.find((item) => item.valu
       </div>
       <van-image :src="store.user?.avatar" />
     </div>
-
     <!-- 医生发的消息 -->
     <div class="msg msg-from" v-if="msgType === MsgType.MsgText && store.user?.id !== from">
       <van-image :src="fromAvatar" />
@@ -83,6 +91,38 @@ const getConsultFlagText = (flag: 0 | 1) => flagOptions.find((item) => item.valu
       <div class="content">
         <div class="time">{{ formatTime(createTime) }}</div>
         <van-image fit="contain" :src="msg.picture?.url" />
+      </div>
+    </div>
+    <!-- 处方 -->
+    <div class="msg msg-recipe" v-if="msgType === MsgType.CardPre">
+      <div class="content" v-if="msg.prescription">
+        <div class="head van-hairline--bottom">
+          <div class="head-tit">
+            <h3>电子处方</h3>
+            <p @click="showPrescription(msg.prescription?.id)">
+              原始处方 <van-icon name="arrow"></van-icon>
+            </p>
+          </div>
+          <p>
+            {{ msg.prescription.name }}
+            {{ msg.prescription.genderValue }}
+            {{ msg.prescription.age }}岁
+            {{ msg.prescription.diagnosis }}
+          </p>
+          <p>开方时间：{{ msg.prescription.createTime }}</p>
+        </div>
+        <div class="body">
+          <div class="body-item" v-for="med in msg.prescription.medicines" :key="med.id">
+            <div class="durg">
+              <p>{{ med.name }} {{ med.specs }}</p>
+              <p>{{ med.usageDosag }}</p>
+            </div>
+            <div class="num">x{{ med.quantity }}</div>
+          </div>
+        </div>
+        <div class="foot">
+          <span @click="buy(msg.prescription)">购买药品</span>
+        </div>
       </div>
     </div>
   </template>
